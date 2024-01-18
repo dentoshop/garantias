bd2169a7227698a9ebaad47e0e8bd7467fb766eb
dentoshop <156879049+dentoshop@users.noreply.github.com>
2024-01-18 05:01:32 +0000

    a

--- Hello.py
+++ Hello.py
@@ -1,9 +1,21 @@
+import os
+from datetime import datetime
+import time
+import pandas as pd
+from copy import copy
+from pathlib import Path
+import re
+import xlwings as xw
+import numpy as np
+import sys
 import streamlit as st
-
+from google.oauth2.credentials import Credentials
+from googleapiclient.discovery import build
+from google.oauth2 import service_account
 tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
 with tab1:
-   st.header("A cat")
+   st.header("A caat")
    st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
 with tab2:

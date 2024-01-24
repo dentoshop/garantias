424875262b2ad838817be8a93152f89724990ab1
dentoshop <156879049+dentoshop@users.noreply.github.com>
2024-01-24 00:00:36 +0000

    a

--- Hello.py
+++ Hello.py
@@ -50,7 +50,7 @@ values = result.get('values',[])
 df=pd.DataFrame(values[1:], columns=values[0])
 df_fix=df[df["ID"] != '']
 df_index=df_fix.index
-maxrow=df_index[-1]+2
+maxrow=df_index[-1]+1
 
 empp, clien = st.tabs(["Empresa","Seguimiento - Cliente"])
 

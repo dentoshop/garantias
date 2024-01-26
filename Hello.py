155eecdf3c8d1691f4e54b801f5b4268ff521471
dentoshop <156879049+dentoshop@users.noreply.github.com>
2024-01-26 06:19:02 +0000

    a

--- Hello.py
+++ Hello.py
@@ -154,11 +154,11 @@ with empp:
             fecha_mod=pd.to_datetime(fecha_mod) 
 
 
-            valores_mod=df_fix[(df_fix['ID'] == row_mod) & (df_fix['NUM_SN'] == num_sn_buscar)]
+            valores_mod=df_fix[(df_fix['ID'] == row_mod) & (df_fix['NUM_SN'] == num_sn_buscar)].tolist()
             
             st.write(row_mod)
             st.write(num_sn_buscar)
-            st.table(valores_mod[0])
+            st.table(valores_mod)
             st.table(df_fix.iloc[row_mod])
 
         with tab3:

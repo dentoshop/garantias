6d62014dd6adf853dbe6277c07b18ace0d0b909f
dentoshop <156879049+dentoshop@users.noreply.github.com>
2024-01-26 07:18:09 +0000

    a

--- Hello.py
+++ Hello.py
@@ -174,7 +174,7 @@ with empp:
             st.write(row_mod)
             
             st.table(df_fix.iloc[row_mod])
-            st.write(valores_mod[18])
+            st.write(valores_mod[17])
 
         with tab3:
             

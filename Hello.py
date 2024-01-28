b566892e1c84f528062a3d26773a3a9a4e318b03
dentoshop <156879049+dentoshop@users.noreply.github.com>
2024-01-28 21:31:01 +0000

    a

--- Hello.py
+++ Hello.py
@@ -221,7 +221,7 @@ with empp:
             boton_mod=st.button("Modificar", on_click=llenar, args=(valores_mod2,RANGE_NAME_MOD,))
 
             a=df_fix.iloc[row_mod]
-            a.fillna('',inplace=True)
+            a.replace(to_replace="null", value='', inplace=True)
             st.table(a)
 
     else: "Contraseña incorrecta"

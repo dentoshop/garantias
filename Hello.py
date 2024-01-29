3b5130001b68f9b494ff30bf6a486f546576e10c
dentoshop <156879049+dentoshop@users.noreply.github.com>
2024-01-29 06:50:43 +0000

    a

--- Hello.py
+++ Hello.py
@@ -264,7 +264,8 @@ with clien:
     df_client=df_fix[df_fix["ID"] == str(id_client)]
     columns_client={"NOMBRE_CLIENTE":"Nombre","NUM_SN":"Numero de Serie","ESTADO":"Estado","FECHA_INGRESO":"Reparacion","FECHA_EVALUACIÓN":"Evaluacion","FECHA_REPAR":"Reparacion","FECHA_LISTO":"Listo para Entrega","FECHA_ENTREGA":"Entregado"}
     df_client_new=pd.DataFrame(df_client[list(columns_client.keys())])
-    df_client_new.rename(columns=columns_client, inplace=True)
+    df_client_new.columns=list(columns_client.values())
+    #df_client_new.rename(columns=columns_client, inplace=True)
     df_client_new.replace(to_replace=['null',None], value='', inplace=True)
     
     st.write(df_client_new.shape)

007ccca52c434519e988c70e2faa9ab1a2727c9f
dentoshop <156879049+dentoshop@users.noreply.github.com>
2024-01-29 06:42:00 +0000

    a

--- Hello.py
+++ Hello.py
@@ -267,7 +267,7 @@ with clien:
     df_client_new.replace(to_replace=['null',None], value='', inplace=True)
     df_client_new.rename(columns=columns_client, inplace=True)
 
-    st.dataframe(df_client_new)
+    st.write(df_client_new)
 
     new_rows=["","",""]
     st.write(list(columns_client.values()))

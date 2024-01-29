1af896f22f367933f1642692385d257cc4c0ccd2
dentoshop <156879049+dentoshop@users.noreply.github.com>
2024-01-29 05:57:17 +0000

    a

--- Hello.py
+++ Hello.py
@@ -250,7 +250,7 @@ with empp:
             a=pd.DataFrame(a)
             a.replace(to_replace=['null',None], value='', inplace=True)
             st.write(type(a))
-            a.style.hide_columns()
+            a.style.set_table_styles([{'selector': 'thead', 'props': [('display', 'none')]}])
 
             st.dataframe( a,use_container_width=st.session_state.use_container_width)
 

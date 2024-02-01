7e178716382de0a6c1dec081ce8b0ffee610d41c
dentoshop <156879049+dentoshop@users.noreply.github.com>
2024-02-01 10:08:58 +0000

    a

--- Hello.py
+++ Hello.py
@@ -312,8 +312,8 @@ with clien:
         df_client_new.columns=["",""]
    
     st.write(df_client_new.iloc[0].values)
-    fig = go.Figure(data=[go.Table(header=dict(values=[df_client_new.transpose().iloc[0].values]),
-                                cells=dict(values=df_client_new.transpose().iloc[1:].values))])
+    fig = go.Figure(data=[go.Table(header=dict(values=[df_client_new.iloc[0].transpose().values]),
+                                cells=dict(values=df_client_new.iloc[1:].transpose().values))])
 
 
     st.plotly_chart(fig)

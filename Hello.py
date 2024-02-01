a8a6d9db13068a664e022f5811ef49501046c48a
dentoshop <156879049+dentoshop@users.noreply.github.com>
2024-02-01 09:52:29 +0000

    a

--- Hello.py
+++ Hello.py
@@ -313,7 +313,7 @@ with clien:
 
     row_heights = {1: 100}  # Change the row index and height as needed
 
-    st.write(df_client_new.columns)
+    st.write(df_client_new.values)
     # Create a Plotly figure
     fig = go.Figure(data=[go.Table(
         header=dict(values=list(df_client_new.columns),

dc288631cc25e83fcbacb844c549a0c2dd21390a
dentoshop <156879049+dentoshop@users.noreply.github.com>
2024-02-01 09:31:12 +0000

    a

--- Hello.py
+++ Hello.py
@@ -299,11 +299,11 @@ with clien:
         else:
             new_rows=new_rows+[""]
 
-
+    st.dataframe(df_client_new)
     df_client_new.loc[len(df_client_new.index)] = new_rows
     df_client_new=df_client_new.T
 
-    st.dataframe('df_client_new')
+    st.dataframe(df_client_new)
     df_client_new.columns=[""," "] 
 
 
